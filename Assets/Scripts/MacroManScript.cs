f2765da25268defb66e89665821b6a116f2c5ec9
Cassidy Saenz <cassidy@clinkle.com>
2014-06-24 16:27:40 -0700

    make the dude move

--- Assets/Scripts/MacroManScript.cs
+++ Assets/Scripts/MacroManScript.cs
@@ -5,6 +5,10 @@ public class MacroManScript : MonoBehaviour {
 
 	private Animator animator;
 
+	public float speed = 10.0f;
+
+	private Vector2 velocity = new Vector2 (0.0f, 0.0f);
+
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator> ();
@@ -15,10 +19,12 @@ public class MacroManScript : MonoBehaviour {
 		float horizontalAxis = Input.GetAxis ("Horizontal");
 		if (horizontalAxis > 0.0f) {
 			animator.SetTrigger ("StartRunning");
+			velocity.x = speed;
 		} else if (horizontalAxis < 0.0f) {
-			// move left
+			velocity.x = -speed;
 		} else {
 			animator.SetTrigger("StopRunning");
+			velocity.x = 0.0f;
 		}
 		if (Input.GetAxis ("Vertical") > 0.0f) {
 			// nothing?
@@ -31,5 +37,9 @@ public class MacroManScript : MonoBehaviour {
 //		if (Input.GetKeyUp (KeyCode.W)) {
 //			animator.SetTrigger ("StopRunning");
 //		}
+	}
+
+	void FixedUpdate () {
+		rigidbody2D.velocity = velocity;
 	}
 }
